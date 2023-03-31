7a308f070887e23d624ef47eeacb2f6235b6d93c
Gabriel Henrique <gabriel.lanzarini@hotmail.com>
2023-03-31 18:26:21 -0300

    Criação da página inicial

---
+++ aplicacao/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/*.{jsx,tsx}", "./src/pages/*.{jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
